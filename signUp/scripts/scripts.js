e7bc6628ec6268fdee8e1ca9682cde6acb3abff7
Yasser Shkeir <shkeiryasser@gmail.com>
2022-09-14 19:41:24 +0300

    Added Sign Up form and variables to JS

---
+++ signUp/scripts/scripts.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  var username = document.getElementById("username");
+  var password = document.getElementById("password");
+  var email = document.getElementById("email");
+  var phone = document.getElementById("phone");
+  var dob = document.getElementById("dob");
+};
