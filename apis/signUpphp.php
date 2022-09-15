5864fb0d004bda861a97a743af38c73cf887a496
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-15 21:13:00 +0300

    checking if username and password are set

--- apis/signUpphp.php
+++ apis/signUpphp.php
@@ -4,5 +4,20 @@
     header("Access-Control-Allow-Headers: *");
     include("connection.php");
 
+
+    if(isset($_GET["username"]))
+    {
+        $username = $_GET["username"];
+    }
+    else {
+        die("username is missing");
+    }
+    if(isset($_GET["password"])){
+        $password = $_GET["password"];
+    }
+    else {
+        die("password is missing");
+    }
+
 ?>
     
