7c24b5bcc8bd10bd4ea549cc7dc6fddbf1a7e292
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-15 21:08:56 +0300

    Adding dummy sign-up API to check on the front end

---
+++ apis/signUpphp.php
@@ -0,0 +1,60 @@
+<?php
+
+    // $username = $_POST["username"];
+    // $pass = $_POST["password"];
+    
+    // $results =[
+    //     "username" => $username,
+    //     "password" => $pass
+    // ];
+
+    // echo json_encode($results);
+
+    // echo json_encode($results);
+    // echo $username
+    // echo $pass
+
+    // if(isset($_POST["phone"]){
+    //     $phone = $_POST["phone"];
+    // }
+    // else {
+    //     die("username, is missing");
+    // }
+    
+    // if(isset($_POST["email"]){
+    //     $email = $_POST["email"];
+    // }
+    // else {
+    //     die("username, is missing");
+    // }
+    // if(isset($_POST["DOB"]){
+    //     $dob = $_POST["DOB"];
+    // }
+    // else {
+    //     die("username, is missing");
+    // }
+    // if(isset($phone))
+    // {
+    //     $stmt = $mysqli -> prepare ("INSERT INTO users (null, username, password, phone, DOB, created_at) VALUES (?, ?, ?, ?, ?)");
+    //     $stmt -> bind_param("ssiss", $username, $password, $phone, $phone, $dob, CURRENT_TIMESTAMP );
+    //     $stmt -> execute();
+    // }
+
+    // else
+    // {
+
+    //     $stmt = $mysqli -> prepare ("INSERT INTO users (null, username, password, phone, DOB, created_at) VALUES (?, ?, ?, ?, ?)");
+    //     $stmt -> bind_param("ssiss", $username, $password, $phone, $phone, $dob, CURRENT_TIMESTAMP );
+    //     $stmt -> execute();
+    // }
+    
+
+    // $response[];
+    // $response["status"] = "sucesss";
+    // echo $response;
+
+    // $stmt -> close();
+    // $mysqli -> close();
+
+    ?>
+    
