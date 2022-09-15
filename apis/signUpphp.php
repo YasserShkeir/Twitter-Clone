206339916c67c42dddcae9c7932c8f678af3cc9d
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-15 21:10:59 +0300

    Adding headers

--- apis/signUpphp.php
+++ apis/signUpphp.php
@@ -1,60 +1,8 @@
 <?php
 
-    // $username = $_POST["username"];
-    // $pass = $_POST["password"];
-    
-    // $results =[
-    //     "username" => $username,
-    //     "password" => $pass
-    // ];
+    header("Access-Control-Allow-Origin:*");
+    header("Access-Control-Allow-Headers: *");
+    include("connection.php");
 
-    // echo json_encode($results);
-
-    // echo json_encode($results);
-    // echo $username
-    // echo $pass
-
-    // if(isset($_POST["phone"]){
-    //     $phone = $_POST["phone"];
-    // }
-    // else {
-    //     die("username, is missing");
-    // }
-    
-    // if(isset($_POST["email"]){
-    //     $email = $_POST["email"];
-    // }
-    // else {
-    //     die("username, is missing");
-    // }
-    // if(isset($_POST["DOB"]){
-    //     $dob = $_POST["DOB"];
-    // }
-    // else {
-    //     die("username, is missing");
-    // }
-    // if(isset($phone))
-    // {
-    //     $stmt = $mysqli -> prepare ("INSERT INTO users (null, username, password, phone, DOB, created_at) VALUES (?, ?, ?, ?, ?)");
-    //     $stmt -> bind_param("ssiss", $username, $password, $phone, $phone, $dob, CURRENT_TIMESTAMP );
-    //     $stmt -> execute();
-    // }
-
-    // else
-    // {
-
-    //     $stmt = $mysqli -> prepare ("INSERT INTO users (null, username, password, phone, DOB, created_at) VALUES (?, ?, ?, ?, ?)");
-    //     $stmt -> bind_param("ssiss", $username, $password, $phone, $phone, $dob, CURRENT_TIMESTAMP );
-    //     $stmt -> execute();
-    // }
-    
-
-    // $response[];
-    // $response["status"] = "sucesss";
-    // echo $response;
-
-    // $stmt -> close();
-    // $mysqli -> close();
-
-    ?>
+?>
     
