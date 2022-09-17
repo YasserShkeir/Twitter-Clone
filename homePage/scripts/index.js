fa27cc67554618e3a9a1f242091dcbb7898ce021
Yasser Shkeir <shkeiryasser@gmail.com>
2022-09-18 02:05:51 +0300

    Edit Left Menu CSS & Add more JS for Profile Edits

--- homePage/scripts/index.js
+++ homePage/scripts/index.js
@@ -6,15 +6,27 @@ window.onload = () => {
 
   /* Profile Section: Profile Tweet/Media/Likes Buttons */
   const profileNavButtons = document.querySelectorAll(".prof-nav-btn");
+  /* Initial State */
+  profileNavButtons[0].style.fontWeight = "600";
+  profileNavButtons[0].style.color = "#fff";
+  profileNavButtons[0].style.borderBottom = "5px solid #1d9bf0";
 
   profileNavButtons.forEach((item) => {
     item.addEventListener("click", (event) => {
+      /* Reset styling to original */
       profileNavButtons.forEach((item) => {
         item.style.border = "none";
         item.style.color = "#71767b";
+        item.style.fontWeight = "400";
       });
+      /* Apply styling to chosen tab */
+      item.style.fontWeight = "600";
       item.style.color = "#fff";
       item.style.borderBottom = "5px solid #1d9bf0";
+
+      if (item === profileNavButtons[0]) {
+        console.log(111);
+      }
     });
   });
 
