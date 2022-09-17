562548520ed00eb22c2dc858ea1104274f9d5c2c
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-18 02:10:23 +0300

    converting to ES6

--- signUp/scripts/scripts.js
+++ signUp/scripts/scripts.js
@@ -1,12 +1,12 @@
-window.onload = function () {
+window.onload = async function () {
   // Get the modal
-  var modal = document.getElementById("myModal");
+  let modal = document.getElementById("myModal");
 
   // Get the button that opens the modal
-  var btn = document.getElementById("signup");
+  let btn = document.getElementById("signup");
 
   // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+  let span = document.getElementsByClassName("close")[0];
 
   // When the user clicks the button, open the modal
   btn.onclick = function () {
@@ -14,30 +14,32 @@ window.onload = function () {
   };
 
   // When the user clicks on <span> (x), close the modal
+
   span.onclick = function () {
     modal.style.display = "none";
   };
 
-  let username = document.getElementById("username");
-  let password = document.getElementById("password");
-  let email = document.getElementById("email");
-  let phone = document.getElementById("phone");
-  let dob = document.getElementById("dob");
+  document.getElementById("submit").addEventListener("click", () =>{
+      
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+    let dob = document.getElementById("dob").value;
+    
+    console.log(username, password, email, dob)
+    fetch("http://localhost:8080/apis/signUp.php", {
 
-  let data = {
-    username: username,
-    password: password,
-    email: email,
-    phone: phone,
-    dob: dob,
-  };
-
-  fetch("", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: { "Content-type": "application/json; charset=UTF-8" },
+      method: "POST",
+      body: new URLSearchParams({
+        "username": username,
+        "password": password,
+        "email": email,
+        "dob": dob
+      })
+    }).then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+      console.log("submit")
+    debugger
   })
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((err) => console.log(err));
 };
