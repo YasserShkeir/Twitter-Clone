16a0f9b361cadcb33284f74309fcbd92835728ed
Yasser Shkeir <shkeiryasser@gmail.com>
2022-09-18 01:49:18 +0300

    Final CSS Edits +JS Edit Profile Nav Buttons Logic

--- homePage/scripts/index.js
+++ homePage/scripts/index.js
@@ -1,4 +1,22 @@
 window.onload = () => {
   let navHome = document.querySelector("nav-home");
   let navProfile = document.querySelector("nav-profile");
+
+  /* Edit Profile Section Start */
+
+  /* Profile Section: Profile Tweet/Media/Likes Buttons */
+  const profileNavButtons = document.querySelectorAll(".prof-nav-btn");
+
+  profileNavButtons.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      profileNavButtons.forEach((item) => {
+        item.style.border = "none";
+        item.style.color = "#71767b";
+      });
+      item.style.color = "#fff";
+      item.style.borderBottom = "5px solid #1d9bf0";
+    });
+  });
+
+  /* Edit Profile Section End */
 };
