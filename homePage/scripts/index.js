813e6e6695b330c91b7021f334cd9a884fbf69d3
Yasser Shkeir <shkeiryasser@gmail.com>
2022-09-18 23:28:26 +0300

    Add Tweet Section JS for multiple Users

--- homePage/scripts/index.js
+++ homePage/scripts/index.js
@@ -133,6 +133,104 @@ window.onload = () => {
 
   rightSection.innerHTML += rightSectionContent;
 
+  /* TWEET FEED */
+
+  const feed = document.querySelector("#tweets-feed");
+
+  let tweetSample = [
+    {
+      tweetInfoHideStatus: "",
+      tweetInfoState: "Followed",
+      tweetInfoUser: "Yasser Shkeir",
+      tweetPosterFName: "Charbel Daoud",
+      tweetPosterUName: "CharbelTheFair",
+      tweetTextContent: "Lorem Ipsum Kan Ya Makan",
+      tweetImgContent: "./assets/icons/default_profile_400x400.png",
+      tweetImgHide: "",
+      tweetLikeCount: "196",
+    },
+    {
+      tweetInfoHideStatus: "hide",
+      tweetInfoState: "",
+      tweetInfoUser: "",
+      tweetPosterFName: "Charbel Daoud",
+      tweetPosterUName: "CharbelTheFair",
+      tweetTextContent: "Assignment 3: Create a Fortune 100 company",
+      tweetImgContent: "",
+      tweetImgHide: "hide",
+      tweetLikeCount: "111",
+    },
+    {
+      tweetInfoHideStatus: "",
+      tweetInfoState: "Liked",
+      tweetInfoUser: "Yasser Shkeir",
+      tweetPosterFName: "Julien Hosri",
+      tweetPosterUName: "Unicorn",
+      tweetTextContent: "Look at this image!",
+      tweetImgContent:
+        "https://img.freepik.com/premium-vector/cute-cartoon-unicorn-pegasus-with-rainbow-mane_116089-138.jpg?w=2000",
+      tweetImgHide: "",
+      tweetLikeCount: "222",
+    },
+  ];
+
+  let tweetLineup = ``;
+
+  const tweetFeed = () => {
+    for (tweet of tweetSample) {
+      tweetLineup += `
+      <div class="tweet">
+      <div class="tweet-info flex ${tweet.tweetInfoHideStatus}">
+      <img
+        src="./assets/icons/5690239_avatar-profile-white-icon-png-transparent-png.png"
+      />
+      ${tweet.tweetInfoState} by <span id="followed-by"> ${tweet.tweetInfoUser}</span>
+    </div>
+    <div class="user-card">
+      <img class="user-card-pfp curr-user-pfp" />
+      <div id="user-card-details">
+        <div id="user-fname">${tweet.tweetPosterFName}</div>
+        <div id="user-uname">@${tweet.tweetPosterUName}</div>
+      </div>
+      <button id="user-card-menu">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M18 14C17.4696 14 16.9609 13.7893 16.5858 13.4142C16.2107 13.0391 16 12.5304 16 12C16 11.4696 16.2107 10.9609 16.5858 10.5858C16.9609 10.2107 17.4696 10 18 10C18.5304 10 19.0391 10.2107 19.4142 10.5858C19.7893 10.9609 20 11.4696 20 12C20 12.5304 19.7893 13.0391 19.4142 13.4142C19.0391 13.7893 18.5304 14 18 14ZM12 14C11.4696 14 10.9609 13.7893 10.5858 13.4142C10.2107 13.0391 10 12.5304 10 12C10 11.4696 10.2107 10.9609 10.5858 10.5858C10.9609 10.2107 11.4696 10 12 10C12.5304 10 13.0391 10.2107 13.4142 10.5858C13.7893 10.9609 14 11.4696 14 12C14 12.5304 13.7893 13.0391 13.4142 13.4142C13.0391 13.7893 12.5304 14 12 14ZM6 14C5.46957 14 4.96086 13.7893 4.58579 13.4142C4.21071 13.0391 4 12.5304 4 12C4 11.4696 4.21071 10.9609 4.58579 10.5858C4.96086 10.2107 5.46957 10 6 10C6.53043 10 7.03914 10.2107 7.41421 10.5858C7.78929 10.9609 8 11.4696 8 12C8 12.5304 7.78929 13.0391 7.41421 13.4142C7.03914 13.7893 6.53043 14 6 14Z"
+            fill="#E7E9EA"
+          />
+        </svg>
+      </button>
+    </div>
+    <div class="tweet-content">
+      <div class="tweet-content-text">${tweet.tweetTextContent}</div>
+      <div class="tweet-content-media flex">
+        <img src="${tweet.tweetImgContent}" class="${tweet.tweetImgHide}" />
+      </div>
+    </div>
+    <div class="tweet-footer flex">
+      <div class="footer-like">
+        <img />
+        <div id="like-count">${tweet.tweetLikeCount}</div>
+      </div>
+      <div class="footer-share flex">
+        <img />
+        Share
+      </div>
+    </div>
+    <div class="line-hori"></div>
+    </div>`;
+    }
+    return tweetLineup;
+  };
+
+  feed.innerHTML += tweetFeed();
+
   /* USER MANAGEMENT START */
 
   /* Profile Pic: */
@@ -228,23 +326,21 @@ window.onload = () => {
   }
 
   /* Edit Profile Section End */
-  function get_tweets(){
-    let username = document.getElementById('username')
-    let tweets =[]
-  
-    fetch("http://localhost:8080/apis/fetch_tweets.php",  {
-  
+  function get_tweets() {
+    let username = document.getElementById("username");
+    let tweets = [];
+
+    fetch("http://localhost:8080/apis/fetch_tweets.php", {
       method: "POST",
       body: new URLSearchParams({
-        "username": username,
-      })
-    }).then(response => response.json())
-      .then(data => {
-        for(d in data){
-  
+        username: username,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        for (d in data) {
         }
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }
- 
 };
